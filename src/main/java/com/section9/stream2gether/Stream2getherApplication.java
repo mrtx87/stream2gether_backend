cafaaa0c9b42cb3ab6a702bb0c03b5192519a553
Stickl <Ralf.Stickl@nwdom.nwdent.de>
2019-12-21 20:43:26 +0100

    init

---
+++ src/main/java/com/section9/stream2gether/Stream2getherApplication.java
@@ -0,0 +1,13 @@
+package com.section9.stream2gether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stream2getherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stream2getherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/section9/stream2gether/Stream2getherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section9.stream2gether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stream2getherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
